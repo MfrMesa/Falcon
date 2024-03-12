7890081ffcc048f208a0e8e78668656c6b0bd8db
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-03-12 01:27:04 +0000

    .

--- Scripts/WeeklySalesScript.js
+++ Scripts/WeeklySalesScript.js
@@ -1,97 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Define sales data for each day
-    var salesData = {
-        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-        datasets: [
-            {
-                label: "Daily Sales",
-                data: [6000, 10000, 7500, 4000, 3500, 5500, 6000],
-                backgroundColor: [
-                    'rgb(44, 123, 229)',
-                    'rgb(44, 123, 229)',
-                    'rgb(44, 123, 229)',
-                    'rgb(44, 123, 229)',
-                    'rgb(44, 123, 229)',
-                    'rgb(44, 123, 229)',
-                    'rgb(44, 123, 229)',
-                ],
-                borderWidth: 0,
-                z: 1,
-            },
-            {
-                label: "Background",
-                data: [10000, 10000, 10000, 10000, 10000, 10000, 10000],
-                backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                borderWidth: 0,
-                z: 0
-            }
-            ]
-    };
+document.addEventListener('DOMContentLoaded', function () {
+  const ctx = document.getElementById('ChartWeeklySalesGraph').getContext('2d');
 
-    // Get canvas element
-    var ctx = document.getElementById('ChartWeeklySalesGraph').getContext('2d');
+  const salesData = {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    datasets: [{
+      label: 'Daily Sales',
+      data: [6000, 10000, 7500, 4000, 3500, 5500, 6000],
+      backgroundColor: 'rgb(44, 123, 229)',
+    },
+    {
+      label: 'Background Bars',
+      data: Array(7).fill(10000),
+      backgroundColor: '#EDF2F9',
+    }],
+  };
 
-    // Create the chart
-    var weeklySalesChart = new Chart(ctx, {
-        type: 'bar',
-        data: salesData,
-        options: {
-            plugins: {
-                legend: {
-                    display: false // Hide legend
-                }
-            },
-            xAxes: [
-                {
-                  stacked: true,
-                },
-            ],
-            yAxes: [
-                {
-                  stacked: false,
-                  ticks: {
-                    beginAtZero: true,
-                  },
-                },
-            ],
-            scales: {
-                x: {
-                    display: false, // Hide x-axis
-                    grid: {
-                        display: false // Hide gridlines
-                    }
-                },
-                y: {
-                    beginAtZero: true,
-                    display: false,
-                    ticks: {
-                        display: false // Hide y-axis labels
-                    },
-                    grid: {
-                        display: false // Hide gridlines
-                    }
-                }
-            },
-            elements: {
-                bar: {
-                    borderWidth: 0, // Hide border
-                    borderRadius: 10, // Set border radius
-                    borderSkipped: 'bottom' // Apply border radius to the lower side of the bars
-                }
-            },
-            tooltips: {
-                backgroundColor: '#000000', // Background color of the tooltip
-                titleFontColor: '#ffffff', // Font color of the title
-                bodyFontColor: '#ffffff', // Font color of the body
-                cornerRadius: 10, // Border radius of the tooltip
-                displayColors: false, // Hide color boxes
-                callbacks: {
-                    label: function(context) {
-                        return '$' + context.raw.toLocaleString() + 'K'; // Format the label as desired
-                    }
-                }
-            }            
-        }
-    });
-});
-
+  let weeklySalesChart = new Chart(ctx, {
+    type: 'bar',
+    data: salesData,
+    options: {
+      animation: false,
+      indexAxis: 'x',
+      barPercentage: 0.3,
+      categoryPercentage: 0.8,
+      maintainAspectRatio: false,
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: false,
+          backgroundColor: '#FFFFFF',
+          borderColor: 'rgba(0,0,0,.1)',
+          borderWidth: 1,
+          titleFontFamily: 'Poppins',
+          bodyFontFamily: 'Poppins',
+          titleColor: '#000000',
+          bodyColor: '#000000',
+        },
+      },
+      scales: {
+        x: {
+          stacked: true,
+          display: false,
+          grid: {
+            display: false,
+          },
+        },
+        y: {
+          display: false,
+          beginAtZero: true,
+          grid: {
+            display: false,
+          }
+        },
+      },
+      elements: {
+        bar: {
+          borderWidth: 0,
+          borderRadius: 5,
+          borderSkipped: 'none',
+        },
+      },
+    },
+  });
+});   
