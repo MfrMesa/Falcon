506607398697280f5e9c09e5858663c91ddca30a
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-03-12 03:39:24 +0000

    Chart progress

--- Scripts/WeeklySalesScript.js
+++ Scripts/WeeklySalesScript.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   const ctx = document.getElementById('ChartWeeklySalesGraph').getContext('2d');
 
-  const salesData = {
-    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    datasets: [{
-      label: 'Daily Sales',
-      data: [6000, 10000, 7500, 4000, 3500, 5500, 6000],
-      backgroundColor: 'rgb(44, 123, 229)',
-    },
-    {
-      label: 'Background Bars',
-      data: Array(7).fill(10000),
-      backgroundColor: '#EDF2F9',
-    }],
-  };
-
   let weeklySalesChart = new Chart(ctx, {
     type: 'bar',
-    data: salesData,
+    data: {
+      labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      datasets: [{
+        label: 'Daily Sales',
+        data: [6000, 10000, 7500, 4000, 3500, 5500, 6000],
+        backgroundColor: 'rgb(44, 123, 229)',
+        hoverBackgroundColor: 'rgba(44, 123, 229',
+      },
+      {
+        label: 'Background Bars',
+        data: Array(7).fill(10000),
+        backgroundColor: '#EDF2F9',
+        hoverBackgroundColor: '#EDF2F9',
+      }],
+    },
     options: {
       animation: false,
       indexAxis: 'x',
@@ -30,14 +30,31 @@ document.addEventListener('DOMContentLoaded', function () {
           display: false,
         },
         tooltip: {
-          enabled: false,
+          enabled: [true, false],
+          position: 'nearest',
+          displayColors: false,
           backgroundColor: '#FFFFFF',
           borderColor: 'rgba(0,0,0,.1)',
           borderWidth: 1,
+          padding: 10,
+          titleFontSize: 16,
+          bodyFontSize: 14,
           titleFontFamily: 'Poppins',
           bodyFontFamily: 'Poppins',
           titleColor: '#000000',
           bodyColor: '#000000',
+          callbacks: {
+            label: (context) => {
+              const { datasetIndex, label, formattedValue } = context;
+              if (datasetIndex === 0) {
+                return `${label}: ${formattedValue.replace(/,/g, '')}`;
+              }
+              return null;
+            },
+            title: function(tooltipItem, data) {
+              return null;
+            },
+          },
         },
       },
       scales: {
@@ -65,4 +82,5 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     },
   });
+  
 });   
